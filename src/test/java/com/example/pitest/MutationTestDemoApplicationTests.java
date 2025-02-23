4a43a12d3b30ed476ab82ed3ca8be4a105d5681e
Jonck van der Kogel <jonckvanderkogel@gmail.com>
2025-02-23 15:30:36 +0100

    added test for application class

--- src/test/java/com/example/pitest/MutationTestDemoApplicationTests.java
+++ src/test/java/com/example/pitest/MutationTestDemoApplicationTests.java
@@ -3,11 +3,19 @@ package com.example.pitest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class MutationTestDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true, "Spring context loaded successfully.");
 	}
 
+	@Test
+	void testMainMethod() {
+		MutationTestDemoApplication.main(new String[] {});
+		assertTrue(true, "Main method executed without exceptions.");
+	}
 }
